95958bd6e4d565108fc0c2e36ce77fcbc900d39d
Alex <Alex@Alexs-MacBook-Pro.local>
2020-08-23 19:37:13 -0400

    modifying file import

--- code/read_file.py
+++ code/read_file.py
@@ -1,3 +1,7 @@
+from tika import parser
+import docx2txt
+import os
+
 def read_txt_file(path: str) -> str:
 	"""
 	Reads a text file as a string.
@@ -19,3 +23,85 @@ def read_txt_file(path: str) -> str:
 		data = file.read()
 	
 	return data
+
+
+def read_pdf_file(path: str) -> str:
+    """
+    Reads a PDF file and extracts the text.
+
+    Parameters 
+    ----------
+    path_to_file : str
+        The path to the file
+
+    Returns
+    -------
+    str
+
+    Examples
+    --------
+
+    """
+    raw = parser.from_file(path)
+
+    text = (raw['content'])
+
+    return text
+    
+
+def read_docx_file(path: str) -> str:
+    """
+    Reads a docx file and extracts the text.
+
+    Parameters 
+    ----------
+    path_to_file : str
+        The path to the file
+
+    Returns
+    -------
+    str
+
+    Examples
+    --------
+
+    """
+    my_text = docx2txt.process(path)
+    
+    return(my_text)
+    
+def check_file_ext(path: str) -> str:
+    """
+    Determines the file extension and extracts the text.
+
+    Parameters 
+    ----------
+    path_to_file : str
+        The path to the file
+
+    Returns
+    -------
+    str
+
+    Examples
+    --------
+
+    """
+    filename, file_extension = os.path.splitext(path)
+
+    if file_extension == '.pdf':
+        data = read_pdf_file(path)
+    elif file_extension == '.docx':
+        data = read_docx_file(path)
+    elif file_extension == '.txt':
+        data = read_txt_file(path)
+    else: 
+        print('Please input either a pdf, docx, or txt file')
+
+    return(data)
+
+
+test = check_file_ext('sample_resume.pdf')
+
+print(test)
+
