49a9594fbfa84cc1955164d8b5f3cb06a69f6511
Alex Chung <56944905+atc2146@users.noreply.github.com>
2020-10-04 20:55:53 -0400

    Minor mods

--- code/main.py
+++ code/main.py
@@ -8,18 +8,18 @@ resume_cleaned = ct.file_prep(resume_string)
 print(resume_cleaned)
 
 #For resume length
-length = cm.get_length_of_resume(resume_cleaned)
-length_rank = cm.resume_length_checker(length[1])
-print('\nResume statistics\n\nYour resume is', length[1], 'words, which is a', length_rank, 'length.')
+length_chars, length_words = cm.get_length_of_resume(resume_cleaned)
+length_eval = cm.resume_length_checker(length_words)
+print(f'\nResume statistics\n\nYour resume is {length_words} words, which is {length_eval}.')
 
 #For measurable metrics
 count_keywords = cm.count_quantifiable_metrics(resume_cleaned)
-print('In addition, it consists of at least',len(count_keywords), 'measurable metrics, which include:\n',count_keywords)
+print(f'In addition, it consists of at least {len(count_keywords)} measurable metrics, which include:\n {count_keywords}.')
 
 #Email check
 email = cm.check_email(resume_cleaned)
-print('Email address is', email[1], 'and it is a', email[0], 'email.')
+print(f'Email address is {email[1]} and it is a {email[0]} email.')
 
 #LinkedIn check
 linkedin = cm.check_linkedin(resume_cleaned)
-print('The resume', linkedin, 'a linked profile address.')
+print(f'The resume {linkedin} a linked profile address.')
